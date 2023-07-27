ed4cf2ae8b8f9b03f61d21dbc8adeb4a0b94d7fe
saroj.dahal <saroj.dahal@lisnepal.com.np>
2023-07-27 18:44:56 +0545

    Second Commit

---
+++ src/portfolio.js
@@ -0,0 +1,71 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://omc.github.io/cleanfolio',
+  title: 'OMC.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'One More Chapter',
+  role: 'book Club',
+  description:
+        'Welcome To our lovely book club. We are very please to have you here. This club prides itself in building the community of fellow book readers and learners. If you want to be part of our growing team, please contact us. Thanks.',
+  resume: '',
+  social: {
+    linkedin: '',
+    github: '',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Project 1',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Redux',
+  'SASS',
+  'Material UI',
+  'Git',
+  'CI/CD',
+  'Jest',
+  'Enzyme',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'sdahal@robling.io',
+}
+
+export { header, about, projects, skills, contact }
